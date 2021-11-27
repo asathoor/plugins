65fcc3f78af7f42f084324e28fee16361f698d33
Per Thykjær Jensen <petj@eaaa.dk>
2021-11-27 09:25:42 +0100

    rem all
    The all will give errors in future PHP versions, so I removed it. TBD in all plugins

--- fontawesome/petj-addFontAwesome.php
+++ fontawesome/petj-addFontAwesome.php
@@ -1,25 +1,18 @@
 <?php
 /*
-Plugin Name: PETJ's add Fontawesome to WordPress
+Plugin Name: add Fontawesome
 Plugin URI: https://github.com/asathoor/plugins
-Description: Add the Fontawesome CSS.
+Description: Will add the CSS for Fontawesome.
 Version: 1.0
 Author: Per Thykjaer Jensen
-Author URI: https://multimusen.dk
+Author URI:https://multimusen.dk
 Textdomain: Pers2020Child
 License: GPLv3
+About this version: Minor bug fix.
 */
-
-function petj_fontawsome(){
-  wp_enqueue_style(
-    'petj-fontawesome',
-    'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css',
-    array(),
-    '4.1.1',
-    all );
+function petj_fontawesome(){
+  wp_enqueue_style('petj-fontawesome', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css', array(), '4.1.1');
   }
 
-add_action(
-  'wp_enqueue_scripts',
-  'petj_fontawsome' );
+add_action('wp_enqueue_scripts', 'petj_fontawesome' );
 ?>
